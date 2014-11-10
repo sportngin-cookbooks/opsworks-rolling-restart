c3c9060322d8922ddd7ba4a43026c33d251c74f5
Andy Fleener <andrew.fleener@sportngin.com>
2014-11-10 08:07:56 -0600

    Update user's authorized_keys file to only append if the file exists

--- recipes/_user.rb
+++ recipes/_user.rb
@@ -25,10 +25,23 @@ directory "#{home_dir}/.ssh" do
   mode 0700
 end
 
-file "#{home_dir}/.ssh/authorized_keys" do
-  owner user
-  group group
-  mode 0700
-  backup false
-  content public_key
-end if public_key
+authorized_keys_file = "#{home_dir}/.ssh/authorized_keys"
+if File.exist?(authorized_keys_file)
+  ruby_block "Add Rolling Restart User's public key" do
+    block do
+      file = Chef::Util::FileEdit.new(authorized_keys_file)
+      file.insert_line_if_no_match(public_key, public_key)
+      file.write_file
+      only_if { public_key }
+    end
+  end
+else
+  file authorized_keys_file do
+    owner user
+    group user
+    mode 0600
+    backup false
+    content public_key
+    only_if { public_key }
+  end
+end
