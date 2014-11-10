180f2f3827ab2e7a65c00dabf88e47e496af35c1
Andy Fleener <andrew.fleener@sportngin.com>
2014-11-10 15:02:57 -0600

    Finish off final tweaks, move the app_restart script into the default recipe, add a deploy recipe to runn rolling_restart

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,2 +1,3 @@
 include_recipe "opsworks-rolling-restart::_user"
 include_recipe "opsworks-rolling-restart::_rolling_restart"
+include_recipe "opsworks-rolling-restart::_app_restart"
---
+++ recipes/deploy.rb
@@ -0,0 +1,1 @@
+execute "#{node[:rolling_restart][:bin_dir]}/#{node[:rolling_restart][:bin]}"
