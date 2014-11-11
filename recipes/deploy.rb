f07e98cdf591dc4587b134f5c0f3c67284a49b54
Andy Fleener <andrew.fleener@sportngin.com>
2014-11-11 11:29:28 -0600

    Switch deploy recipe to log all output from the rolling_restart command

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,1 +1,8 @@
-execute "#{node[:rolling_restart][:bin_dir]}/#{node[:rolling_restart][:bin]}"
+ruby_block "Rolling Restart" do
+  block do
+    cmd = Mixlib::ShellOut.new("#{node[:rolling_restart][:bin_dir]}/#{node[:rolling_restart][:bin]}")
+    cmd.run_command
+    cmd.error!
+    [cmd.stderr, cmd.stdout].join("\n")
+  end
+end
