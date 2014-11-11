e2ad035ab4b884dec801ed8abaf487401793001d
Andy Fleener <andrew.fleener@sportngin.com>
2014-11-11 11:37:44 -0600

    Actually log output

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -2,7 +2,7 @@ ruby_block "Rolling Restart" do
   block do
     cmd = Mixlib::ShellOut.new("#{node[:rolling_restart][:bin_dir]}/#{node[:rolling_restart][:bin]}")
     cmd.run_command
+    Chef::Log.info([cmd.stderr, cmd.stdout].join("\n"))
     cmd.error!
-    [cmd.stderr, cmd.stdout].join("\n")
   end
 end
