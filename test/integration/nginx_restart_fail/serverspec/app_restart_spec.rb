d9a2edbba0678986bcb4caeda146521071996036
Elliot Hursh <elliothursh@gmail.com>
2015-02-13 11:27:02 -0600

    refactoring templates, adding more potential commands

--- test/integration/nginx_restart_fail/serverspec/app_restart_spec.rb
+++ test/integration/nginx_restart_fail/serverspec/app_restart_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
+describe file('/usr/local/bin/app-restart') do
+  it { should be_file }
+  it { should contain "RETRIES=10" }
+  it { should contain "PORT=81" }
+  it { should contain "sleep 10" }
+  it { should contain "curl" }
+  it { should contain "haproxy-add" }
+  it { should contain "haproxy-remove" }
+  it { should contain "nginx restart" }
+  it { should contain "app_ready"}
+  it { should contain "before_command"}
+  it { should contain "after_command"}
+end
+
 describe command("/usr/local/bin/app-restart") do
   its(:exit_status) { should eq 1 }
 end
