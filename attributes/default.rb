b40b7682be445f9b309da37062926ad0cd0612e4
matthewrkrieger <matt.krieger@sportngin.com>
2015-10-13 15:37:29 -0500

    added timeout attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,6 +6,7 @@ default[:rolling_restart][:ssh][:group] = 'opsworks'
 default[:rolling_restart][:bin_dir] = '/usr/local/bin'
 default[:rolling_restart][:template] = 'rolling-restart.sh.erb'
 default[:rolling_restart][:bin] = 'rolling-restart'
+default[:rolling_restart][:timeout] = 900
 
 default[:app_restart][:load_balancer_ip] = nil
 
