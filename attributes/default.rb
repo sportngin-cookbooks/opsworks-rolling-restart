2cbb3ce1e3e8b2e91eb405e0c8f1d0b9a6feaaec
newzac <zachary.newman@sportngin.com>
2016-12-12 15:59:53 -0600

    set rolling restart attributes based on lb type

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,14 +14,25 @@ default[:app_restart][:elb_load_balancer] = nil
 
 default[:app_restart][:bin_dir] = '/usr/local/bin'
 
-default[:app_restart][:add_bin] = 'haproxy-add'
-default[:app_restart][:add_template] = 'haproxy-add.sh.erb'
-
-default[:app_restart][:remove_bin] = 'haproxy-remove'
-default[:app_restart][:remove_template] = 'haproxy-remove.sh.erb'
-
 default[:app_restart][:restart_bin] = 'app-restart'
 default[:app_restart][:restart_template] = 'app-restart.sh.erb'
+
+if node[:rolling_restart][:load_balancer_type] == 'elb'
+  default[:external_load_balancer] = true
+
+  default[:app_restart][:add_bin] = 'elb-add'
+  default[:app_restart][:add_template] = 'elb-add.sh.erb'
+
+  default[:app_restart][:remove_bin] = 'elb-remove'
+  default[:app_restart][:remove_template] = 'elb-remove.sh.erb'
+else
+  default[:app_restart][:add_bin] = 'haproxy-add'
+  default[:app_restart][:add_template] = 'haproxy-add.sh.erb'
+
+  default[:app_restart][:remove_bin] = 'haproxy-remove'
+  default[:app_restart][:remove_template] = 'haproxy-remove.sh.erb'
+end
+
 
 default[:app_restart][:app_running_command] = 'curl --silent --fail --max-time 5 127.0.0.1:$PORT/okcomputer'
 default[:app_restart][:finished_requests_command] = 'sleep 10'
