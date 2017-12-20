4ee54631e5dc39877cee8a04b8c1ab69564ad16e
Emma Sax <emma.sax4@gmail.com>
2017-12-20 15:54:51 -0600

    Use a Chef 12 version for getting the load balancer

--- libraries/rolling_restart.rb
+++ libraries/rolling_restart.rb
@@ -1,19 +1,23 @@
 module RollingRestart
   module Helpers
+    def chef_11?
+      node[:opsworks]
+    end
+
     def app_instances
-      if node[:opsworks]
+      if chef_11?
         node[:opsworks][:layers].map { |layer_name, layer_attrs|
           layer_attrs[:instances] if layer_name.to_s.include?("app")
         }.compact.flatten(1).reduce(&:merge) # Flatten down the list of instances to a hash of { hostname: instance_data }
       else
-        app_layer = search("aws_opsworks_layer").select{ |l| l['shortname'].include?("app") }
+        app_layer = search("aws_opsworks_layer").select{ |l| l['shortname'].include?("app") }.first
         layer_id = app_layer['layer_id']
         instances = search("aws_opsworks_instance").select{ |i| i['layer_ids'].include?(layer_id) }.compact.flatten(1).reduce(&:merge)
       end
     end
 
     def instances
-      if node[:opsworks]
+      if chef_11?
         node[:opsworks][:layers].map { |layer_name, layer_attrs| layer_attrs[:instances] }.compact.flatten(1).reduce(&:merge)
       else
         instances = search("aws_opsworks_instance")
@@ -21,13 +25,19 @@ module RollingRestart
     end
 
     def load_balancer
-      instances.detect{|hostname, data|
-        data[:elastic_ip] && !data[:elastic_ip].empty?
-      }.last
+      if chef_11?
+        instances.detect{ |hostname, data|
+          data[:elastic_ip] && !data[:elastic_ip].empty?
+        }.last
+      else
+        instances.detect{ |hostname, data|
+          data['elastic_ip'] != "null"
+        }.last
+      end
     end
 
     def elb_load_balancer
-      if node[:opsworks]
+      if chef_11?
         node[:opsworks][:stack][:'elb-load-balancers'].first
       else
         search("aws_opsworks_elastic_load_balancer").first
